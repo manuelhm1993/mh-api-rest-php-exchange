e4d954697b34f73f00a53bb8228a799c2bcd19b1
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-10 02:11:39 -0400

    Frontend consumiendo el backend con fetch, delegación de eventos y templates html

--- V1/frontend/assets/js/app.js
+++ V1/frontend/assets/js/app.js
@@ -1,6 +1,12 @@
+// Declaración de variables
+const content = document.querySelector('#content');
+const contentTemplate = document.querySelector('#content-template').content;
+const fragment = new DocumentFragment();
+
+// Hace la petición para consumir la API
 const fetchAPI = async (url) => {
     try {
-        const res = await fetch(url);
+        const res  = await fetch(url);
         const data = await res.json();
         
         render(data);
@@ -10,15 +16,36 @@ const fetchAPI = async (url) => {
     }
 };
 
+// Renderiza el contenido con la data
 const render = (data) => {
-    document.querySelector('#datos').innerHTML = `
-        - Dolar: ${data.dolar}
-        - Euro: ${data.euro}
-    `;
+    content.textContent = '';
+
+    data.forEach(registro => {
+        const clone = contentTemplate.cloneNode(true);
+        const spans = clone.querySelectorAll('li span');
+
+        // Devuelve un array con las claves para corresponderlas con los índices de los spans
+        const keys = Object.keys(registro);
+
+        spans.forEach((span, i) => {
+            // Con un solo bucle se corresponden los spans con los campos de la tabla
+            span.textContent = registro[keys[i]];
+        });
+
+        fragment.appendChild(clone);
+    });
+
+    content.appendChild(fragment);
 };
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    const url = 'http://localhost/cursos/API-API-REST/PHP/mh-api-rest-php-exchange/V1/backend/datos.php';
+// Delegación de eventos
+//
+// Al hacer click
+document.addEventListener('click', (e) => {
+    const fuenteEvento = e.target;
+    const url = 'http://localhost/cursos/API-API-REST/PHP/mh-api-rest-php-exchange/V1/backend/';
 
-    fetchAPI(url);
+    if(fuenteEvento.id === 'euro' || fuenteEvento.id === 'dolar') {
+        fetchAPI(url + fuenteEvento.id);
+    }
 });
