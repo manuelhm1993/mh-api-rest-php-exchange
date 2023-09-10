1d54c18b9fabb03c58eb9f44fc5fb8b9d1ef101c
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-10 01:14:27 -0400

    Backend para servir los datos de las distintas divisas terminado

---
+++ V1/backend/database/request.php
@@ -0,0 +1,24 @@
+<?php
+
+$selectDivisa = function(string $request) use ($link, $user, $password, $cerrarConexiones) {
+    try {
+        $pdo = new PDO($link, $user, $password);
+    
+        // ---------------- Hacer una consulta a la tabla de divisa seleccionada
+        $sql = "SELECT * FROM $request";
+    
+        $stm = $pdo->prepare($sql);
+        $stm->execute();
+    
+        $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    } 
+    catch (PDOException $e) {
+        $result = $e->getMessage();
+    }
+    finally {
+        $cerrarConexiones($pdo, $stm);
+    }
+
+    echo json_encode($result);
+};
+
--- V1/backend/datos.php
+++ V1/backend/datos.php
@@ -1,10 +1,19 @@
 <?php
 
-$divisa = $_GET['divisa'];
+require_once __DIR__."/config/variables-conexion.php";
+require_once __DIR__."/database/globals.php";
+require_once __DIR__."/database/request.php";
 
-$datos = [
-    'dolar' => 40,
-    'euro'  => 45,
-];
-
-echo json_encode($divisa);
+if(isset($_GET['divisa'])) {
+    switch($_GET['divisa']) {
+        case 'euro':
+            $selectDivisa('euro');
+            break;
+        case 'dolar':
+            $selectDivisa('dolar');
+                break;
+        default:
+            echo json_encode(['error' => 'Url no válida']);
+            break;
+    }
+}
